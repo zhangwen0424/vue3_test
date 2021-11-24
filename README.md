e9000645984c76e450ad95988f5d82d21f74e7a6
mornki <1048515550@qq.com>
2021-11-24 16:32:12 +0800

    vue3

---
+++ README.md
@@ -0,0 +1,14 @@
+# vue3_test
+
+## 创建项目
+
+1. 创建vue3需要先更新vue-cli脚手架4.5版本
+sudo cnpm i -g @vue/cli
+2. 创建项目
+vue create vue3_test
+3. 进入项目
+cd vue3_test
+4. 初始化项目
+yarn install
+5. 启动项目
+yarn serve
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+/*
+ * @Date: 2021-11-24 16:27:50
+ * @LastEditors: zhangwen
+ * @LastEditTime: 2021-11-24 16:27:51
+ * @FilePath: /vue3_test/vue.config.js
+ */
+module.exports = {
+  lintOnSave: false
+}
